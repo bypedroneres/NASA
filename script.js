29f03e74015a4bf65732195d600d7a6ef372fbe9
Pedro Neres <124516595+bypedroneres@users.noreply.github.com>
2023-07-21 22:00:10 -0300

    updated date

--- script.js
+++ script.js
@@ -1,43 +1,48 @@
-const apiKey = "BnjlgCuASb6JOgvfmmMWhtw5TN9eiRyuD8sCTxJa";
-const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
-
 // Function to fetch the NASA image of the day data
 async function fetchAPOD() {
-  try {
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error("Failed to fetch data from NASA API");
+    const apiKey = "BnjlgCuASb6JOgvfmmMWhtw5TN9eiRyuD8sCTxJa";
+    const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+  
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error("Failed to fetch data from NASA API");
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
     }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error(error);
   }
-}
-
-// Function to display the NASA image of the day data
-function displayAPOD(data) {
-  const apodImage = document.getElementById("apodImage");
-  const apodTitle = document.getElementById("apodTitle");
-  const apodDate = document.getElementById("apodDate");
-  const apodExplanation = document.getElementById("apodExplanation");
-
-  apodImage.src = data.url;
-  apodImage.alt = data.title;
-  apodTitle.textContent = data.title;
-
-  // Format the date (YYYY-MM-DD) to a more readable format (e.g., July 21, 2023)
-  const date = new Date(data.date);
-  const options = { year: "numeric", month: "long", day: "numeric" };
-  const formattedDate = date.toLocaleDateString("en-US", options);
-  apodDate.textContent = formattedDate;
-
-  apodExplanation.textContent = data.explanation;
-}
-
-// Call the function to fetch the data and display it on the page
-fetchAPOD().then((data) => {
-  if (data) {
-    displayAPOD(data);
+  
+  // Function to format the date to a more readable format (e.g., July 21, 2023)
+  function formatDate(date) {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return date.toLocaleDateString("en-US", options);
   }
-});
+  
+  // Function to display the NASA image of the day data
+  function displayAPOD(data) {
+    const apodImage = document.getElementById("apodImage");
+    const apodTitle = document.getElementById("apodTitle");
+    const apodDate = document.getElementById("apodDate");
+    const apodExplanation = document.getElementById("apodExplanation");
+  
+    apodImage.src = data.url;
+    apodImage.alt = data.title;
+    apodTitle.textContent = data.title;
+  
+    const date = new Date();
+    const formattedDate = formatDate(date);
+    apodDate.textContent = formattedDate;
+  
+    apodExplanation.textContent = data.explanation;
+  }
+  
+  // Call the function to fetch the data and display it on the page
+  fetchAPOD().then((data) => {
+    if (data) {
+      displayAPOD(data);
+    }
+  });
+  
